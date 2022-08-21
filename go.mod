555a14d2b66119881002b7848bbde2bff6fa7365
Melvin Lammerts <melvinrts@gmail.com>
2022-08-21 20:25:06 +0200

    v1

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/melvinsh/unhttpx
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net/url"
+	"os"
+)
+
+func main() {
+	var urls []string
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for scanner.Scan() {
+		u := scanner.Text()
+
+		if u != "" {
+			urls = append(urls, u)
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Println(err)
+	}
+
+	for _, url := range urls {
+		fmt.Println(host(url))
+	}
+}
+
+func host(u string) string {
+	parsed, err := url.Parse(u)
+
+	if err != nil {
+		return u
+	}
+
+	return parsed.Host
+}
